e21550bcddc3c460139b463d66fc45113c6bb2d0
Bhanu Prakash <bhnprksh222@gmail.com>
2025-12-01 22:39:09 -0500

    src changed for img

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,3 @@
 console.log("DevOps Final Project Loaded Successfully!");
 
-document.addEventListener("DOMContentLoaded", () => {
-    const img = document.getElementById("backend-image");
-    img.src = "http://localhost:5001/image";
-});
 
