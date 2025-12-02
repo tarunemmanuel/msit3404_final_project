dc8b39ff3e1c29b71d9cbad22e91d5f395b42b57
Bhanu Prakash <bhnprksh222@gmail.com>
2025-12-01 21:10:49 -0500

    change in the port

--- frontend/script.js
+++ frontend/script.js
@@ -2,6 +2,6 @@ console.log("DevOps Final Project Loaded Successfully!");
 
 document.addEventListener("DOMContentLoaded", () => {
     const img = document.getElementById("backend-image");
-    img.src = "http://localhost:5000/image";
+    img.src = "http://localhost:5001/image";
 });
 
